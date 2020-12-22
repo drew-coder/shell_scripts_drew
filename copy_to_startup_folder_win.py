9a29063732b87906880545b349de3bb7146a94ff
drew-coder <73308954+drew-coder@users.noreply.github.com>
2020-12-22 23:29:27 +0300

    startup script
    Copies ANY script to the windows startup folder.

---
+++ copy_to_startup_folder_win.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import shutil
+import getpass
+user=getpass.getuser()
+shutil.copy2("name-of-file-to-copy.pyw", "C:\Users\\"+user+"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup")
